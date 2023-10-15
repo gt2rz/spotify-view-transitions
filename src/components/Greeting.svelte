e61a6056b4408f0a2c0348c202aada80b6fbc27d
Miguel Gutierrez <gt2rz.dev@gmail.com>
2023-10-15 18:32:29 -0500

    initial

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  const currentTime = new Date();
+  const currentHour = currentTime.getHours();
+
+  let greeting = "Good morning";
+
+  if (currentHour > 12 && currentHour < 18) {
+    greeting = "Good afternoon";
+  } else if (currentHour >= 18 && currentHour < 24) {
+    greeting = "Good evening";
+  }
+</script>
+
+<h1 class="text-3xl font-bold">{greeting}</h1>
